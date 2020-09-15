421ff15bedf178090e6413c001d5033c25f3e671
Sam Ruh <ruh.samuel@gmail.com>
2020-09-15 12:21:07 -0500

    created fork

--- finished-application/backend/src/createServer.js
+++ finished-application/backend/src/createServer.js
@@ -1,13 +1,13 @@
-const { GraphQLServer } = require('graphql-yoga');
-const Mutation = require('./resolvers/Mutation');
-const Query = require('./resolvers/Query');
-const db = require('./db');
+const { GraphQLServer } = require("graphql-yoga");
+const Mutation = require("./resolvers/Mutation");
+const Query = require("./resolvers/Query");
+const db = require("./db");
 
 // Create the GraphQL Yoga Server
 
 function createServer() {
   return new GraphQLServer({
-    typeDefs: 'src/schema.graphql',
+    typeDefs: "src/schema.graphql",
     resolvers: {
       Mutation,
       Query,
@@ -15,7 +15,7 @@ function createServer() {
     resolverValidationOptions: {
       requireResolversForResolveType: false,
     },
-    context: req => ({ ...req, db }),
+    context: (req) => ({ ...req, db }),
   });
 }
 
